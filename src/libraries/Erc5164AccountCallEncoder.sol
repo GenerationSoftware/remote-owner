fe8556bacc8ba664010f7a0c3372fe25e0fb3f23
Brendan Asselstine <mail.asselstine@gmail.com>
2023-07-27 19:05:00 -0700

    Changed encoder bytes data to memory

--- src/libraries/Erc5164AccountCallEncoder.sol
+++ src/libraries/Erc5164AccountCallEncoder.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import { Erc5164Account } from "../Erc5164Account.sol";
 
 library Erc5164AccountCallEncoder {
-    function encodeCalldata(address target, uint256 value, bytes calldata data) internal pure returns (bytes memory) {
+    function encodeCalldata(address target, uint256 value, bytes memory data) internal pure returns (bytes memory) {
         return abi.encodeWithSelector(
             Erc5164Account.execute.selector,
             target,
